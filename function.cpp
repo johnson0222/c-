194fde555d2f3d9ae6a4c25d6e367da21b417246
MZ <hijohnson9105757@gmail.com>
2022-04-22 15:40:31 +0800

    4/22 practice function

---
+++ function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+void star()
+ {
+ 	int j;
+ 	for(j=1;j<=8;j++)
+ 		cout << "*";
+ 	cout << endl;
+ }
+ 
+int main()
+{
+ star();
+ cout << "6*6=" << 6*6 << endl;
+ star(); 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
