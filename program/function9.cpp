d6e7bcad0df3711095a281a701eeb7287a5d4828
MZ <hijohnson9105757@gmail.com>
2022-04-26 14:23:50 +0800

    global variabel and use to extern approach

---
+++ program/function9.cpp
@@ -0,0 +1,33 @@
+//https://shengyu7697.github.io/cpp-extern/ ---->參考link 
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+void peri(double),area(double);
+
+double pi=3;
+
+main()
+{
+	//double pi = 3;
+	extern double pi; //extern 抓取全域變數的value 
+	double r = 2.0;
+	cout << "pi=" << pi << endl;
+	cout <<"radius=" << r <<endl;
+	peri(r);
+	area(r); 
+}
+
+//-------global variable-------
+//double pi = 3.14;
+void peri(double r)
+{
+	cout <<"peripheral length=" << 2*pi*r << endl;
+	return; 
+}
+void area (double r)
+{
+	cout << "area=" << pi*r << endl;
+	return; 
+}
